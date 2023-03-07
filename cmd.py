abfbf163cab29a4e9230cbfe92794e015a2330e7
Aditya Seth <adityaseth.cse@gmail.com>
2023-03-07 19:38:31 +0530

    okay

---
+++ cmd.py
@@ -0,0 +1,1 @@
+gcc -c main.c -o main.o
--- my_program.c
+++ my_program.c
@@ -15,13 +15,6 @@ int main() {
     }
     printf("\n");
 
-    // Find the maximum element in the array
-    int max = find_max(arr, n);
-    printf("Maximum element: %d\n", max);
-
-    // Find the minimum element in the array
-    int min = find_min(arr, n);
-    printf("Minimum element: %d\n", min);
-
+    
     return 0;
 }
